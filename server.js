28fa7641051acd2617e61626803d35516209135a
unknown <ismayiloff522@gmail.com>
2024-08-28 18:44:56 +0400

    change dotenv

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 });
 
 // Server Setup
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
